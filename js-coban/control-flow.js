255ab1b91526fa6040b2b85e56eb2baed8ea10f3
PhamHuuThach <thachhthlqt@gmail.com>
2025-02-10 16:51:26 +0700

    Start

---
+++ js-coban/control-flow.js
@@ -0,0 +1,95 @@
+//Toán tử logic(AND-&&. OR-||, NOT-!)
+var name = "thach"; // Biến nâng cấp
+var age = 20; // Biến nâng cấp
+var isMarried = true; // Biến nâng cấp
+var isStudent = false; // Biến nâng cấp
+console.log(name && age && isMarried && isStudent);
+// Output: false
+console.log(name || age || isMarried || isStudent);
+// Output: thach
+console.log(!isMarried);
+// Output: false
+console.log(!isStudent);
+// Output: true
+
+// Câu đièu kiện if else
+var name = "thach";
+var age = 20;
+var isMarried = false;
+if (isMarried) {
+  console.log(name + " is married");
+} else {
+  console.log(name + " is not married");
+}
+
+// Câu điều kiện switch case
+var name = "thach";
+var age = 20;
+var isMarried = false;
+switch (true) {
+  case isMarried:
+    console.log(name + " is married");
+    break;
+  default:
+    console.log(name + " is not married");
+    break;
+}
+
+// Câu điều kiện while
+var i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+// Câu điều kiện for
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+// Câu điều kiện for of
+var arr = [1, 2, 3, 4, 5];
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+  var item = arr_1[_i];
+  console.log(item);
+}
+// Câu điều kiện for in
+var obj = {
+  name: "thach",
+  age: 20,
+  isMarried: false,
+};
+for (var key in obj) {
+  console.log(key + ": " + obj[key]);
+}
+
+/*Falsy and Truthy
+Falsy: 0, '', undefined, null, NaN,false
+Truthy: All other values
+*/
+//Falsy: 0
+console.log(!!0);
+//Falsy: ''
+console.log(!!"");
+//Falsy: undefined
+console.log(!!undefined);
+//Falsy: null
+console.log(!!null);
+//Falsy: NaN
+console.log(!!NaN);
+//Falsy: false
+console.log(!!false);
+
+//Truthy: All other values
+console.log(!!"thach");
+console.log(!!1);
+console.log(!!true);
+console.log(!!{});
+console.log(!![]);
+console.log(!!function () {});
+
+// Câu điều kiện ternary
+var name = "thach";
+var age = 20;
+var isMarried = false;
+isMarried
+  ? console.log(name + " is married")
+  : console.log(name + " is not married");
---
+++ js-coban/variables_and_data-type.js
@@ -0,0 +1,22 @@
+// Primitive Data Types
+/* 
+    1. Number
+    2. String
+    3. Boolean (so sánh cá toán hạng)
+    4. Null
+    5. Undefined
+    kiểm tra kiểu dữ liệu của biến sử dụng typeof
+*/
+var name = "thach";
+var age = 20;
+console.log("name: " + name, "tuoi: " + age);
+
+var isMarried;
+isMarried = true; // Kiểu dữ liệu luận lý
+console.log("isMarried: " + isMarried);
+
+// Đột biến giá trị(Variables mutations)
+age = "teo";
+console.log("tuoi: " + age);
+
+// alert(name + " " + age + " " + isMarried);
